1ff5ed8ef39e2a664b3c8c0ac1f280a176d7f104
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2025-12-17 14:00:36 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    static int income = 100_000;
+    static int spending = 40_000;
+
+    public static void main(String[] args) {
+        System.out.println("Aleksandr Menshchikov!");
+        System.out.println("Итого (руб):");
+        System.out.println(income - spending);
+    }
+}
