e743e75726257f16e103246c94148f84e2ca7844
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2025-12-18 11:05:27 +0300

    move static fields to method main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,9 @@
 package org.example;
 
 public class Main {
-    static int income = 100_000;
-    static int spending = 40_000;
-
     public static void main(String[] args) {
+        int income = 100_000;
+        int spending = 40_000;
         System.out.println("Aleksandr Menshchikov!");
         System.out.println("Итого (руб):");
         System.out.println(income - spending);
